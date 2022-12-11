f4b081f4b1921c76f1d64a404fb62bbc47e3f2dc
malikShahabBhutta <73873661+malikShahabBhutta@users.noreply.github.com>
2022-12-12 01:36:51 +0500

    styleFile

--- javascript.js
+++ javascript.js
@@ -8,3 +8,12 @@ function clickHandler(){
     console.log(output.innerText="shahahahh"+textinput.value);
 }
 btntranslate.addEventListener("click", clickHandler);
+
+var year=0;
+var input=("Enter year");
+if(input==4%0){
+    conslo.log("leap Year");
+}
+else{
+    console.log("Not Leap Year");
+}
